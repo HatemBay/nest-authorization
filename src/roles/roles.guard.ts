c42607126ffdb047f9f03436b060048ebb8c339d
hatemBay <bayoudhhatem5@gmail.com>
2023-07-22 19:22:22 +0100

    added guard for role authorization

---
+++ src/roles/roles.guard.ts
@@ -0,0 +1,11 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    return true;
+  }
+}
