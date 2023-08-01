14c42ed5ed0e22e41d000cfcf1c69035f4f425f2
hatemBay <bayoudhhatem5@gmail.com>
2023-08-01 15:59:41 +0100

    changed roles from model to DTO

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -14,7 +14,11 @@ export class CreateUserDto {
   @IsNotEmpty()
   password: string;
 
-  // I used a dto rather than the very module because in testing it would require me to include the id
-  // this is subject to change as I learn mroe about the topic
+  // * I used a dto rather than the very module because in testing it would require me to include the id
+  // * this is subject to change as I learn moree about the topic
+
+  // * -> So apparently calling the DTO of the object in relation with our class is better than calling it directly
+  // * which makes sense since we are going to CREATE that object with our class which would optimally call for
+  // * its create DTO
   roles: CreateRoleDto[];
 }
