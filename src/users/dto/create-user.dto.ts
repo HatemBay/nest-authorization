628f6a24b3a9758e1581f73c7b4b8f94715d00ab
hatemBay <bayoudhhatem5@gmail.com>
2023-08-01 15:00:56 +0100

    temporary change to roles, refer to comments

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,13 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { CreateRoleDto } from 'src/roles/dto/create-role.dto';
 import { Role } from 'src/roles/entities/role.entity';
-import { Column } from 'typeorm';
 
 export class CreateUserDto {
   @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
   username: string;
 
   @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
   password: string;
 
-  roles: Role[];
+  // I used a dto rather than the very module because in testing it would require me to include the id
+  // this is subject to change as I learn mroe about the topic
+  roles: CreateRoleDto[];
 }
