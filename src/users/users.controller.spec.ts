527a7ec7663bb3400a561e06ed82621097401e9c
hatemBay <bayoudhhatem5@gmail.com>
2023-08-01 15:55:43 +0100

    added user controller testing/ mocked the userService dependency/ added tests to create method

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -5,16 +5,46 @@ import { UsersService } from './users.service';
 describe('UsersController', () => {
   let controller: UsersController;
 
+  const mockUsersService = {
+    create: jest.fn((dto) => {
+      return {
+        id: Date.now(),
+        ...dto,
+      };
+    }),
+  };
+  const mockRole = () => ({
+    findAndCountAll: jest.fn(),
+  });
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
       providers: [UsersService],
-    }).compile();
+    })
+      .overrideProvider(UsersService)
+      .useValue(mockUsersService)
+      .compile();
 
     controller = module.get<UsersController>(UsersController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create a user', async () => {
+    expect(
+      await controller.create({
+        username: 'Hatem',
+        password: '123',
+        roles: [{ name: 'ADMIN' }],
+      }),
+    ).toEqual({
+      id: expect.any(Number),
+      username: 'Hatem',
+      password: '123',
+      roles: [{ name: 'ADMIN' }],
+    });
   });
 });
