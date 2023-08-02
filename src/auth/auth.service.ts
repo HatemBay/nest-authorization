74d69175c6836760dec70d8cf6bdf099fd3b3ad8
hatemBay <bayoudhhatem5@gmail.com>
2023-08-02 18:38:03 +0100

    changed the UsersService path to relative

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { User } from 'src/users/entities/user.entity';
-import { UsersService } from 'src/users/users.service';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
