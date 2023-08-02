0cd8dcf734251e0205280a8b59b5562e1fb4d91d
hatemBay <bayoudhhatem5@gmail.com>
2023-08-02 17:40:05 +0100

    added tests to the user service methods, added to the users repository mock

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -6,11 +6,42 @@ import { User } from './entities/user.entity';
 describe('UsersService', () => {
   let service: UsersService;
 
+  const dto = {
+    username: 'Hatem',
+    password: '123',
+    roles: [{ name: 'ADMIN' }],
+  };
+
   const mockUsersRepository = {
     create: jest.fn().mockImplementation((dto) => dto),
-    save: jest.fn().mockImplementation((dto) => {
-      return {
+    save: jest.fn().mockImplementation(async (dto) => {
+      return await {
         id: Date.now(),
+        ...dto,
+      };
+    }),
+    find: jest.fn().mockImplementation(async () => {
+      return await [
+        {
+          id: Date.now(),
+          ...dto,
+        },
+      ];
+    }),
+    findOneOrFail: jest.fn().mockImplementation(async ({ where }) => {
+      const param = where.id || where.username;
+      try {
+        return await {
+          param,
+          ...dto,
+        };
+      } catch (err) {
+        throw err;
+      }
+    }),
+    remove: jest.fn().mockImplementation(async (id) => {
+      return await {
+        id,
         ...dto,
       };
     }),
@@ -35,12 +66,6 @@ describe('UsersService', () => {
   });
 
   it('should create a new user record and return it', async () => {
-    const dto = {
-      username: 'Hatem',
-      password: '123',
-      roles: [{ name: 'ADMIN' }],
-    };
-
     expect(await service.create(dto)).toEqual({
       id: expect.any(Number),
       ...dto,
@@ -48,5 +73,53 @@ describe('UsersService', () => {
 
     // * Asserting that the create and save methods are called would probably cause problems
     // * if we used insert from the repository instead of create, so better not assert
+  });
+
+  it('should return all users', async () => {
+    expect(await service.findAll()).toEqual([
+      {
+        id: expect.any(Number),
+        ...dto,
+      },
+    ]);
+  });
+
+  it('should find one user by its id', async () => {
+    expect(await service.findOneById(1)).toEqual({
+      id: 1,
+      ...dto,
+    });
+
+    expect(mockUsersRepository.findOneOrFail).toHaveBeenCalledWith({
+      where: { id: 1 },
+    });
+  });
+
+  it('should find one user by its username', async () => {
+    expect(await service.findOneByUsername('Hatem')).toEqual({
+      id: 1,
+      ...dto,
+    });
+
+    expect(mockUsersRepository.findOneOrFail).toHaveBeenCalledWith({
+      where: { username: 'Hatem' },
+    });
+  });
+
+  it('should find a user by its id then update its information and return it', async () => {
+    expect(await service.update(1, dto)).toEqual({
+      id: 1,
+      ...dto,
+    });
+
+    expect(mockUsersRepository.findOneOrFail).toHaveBeenCalledWith(1);
+    expect(mockUsersRepository.save).toHaveBeenCalled();
+  });
+
+  it('should remove a user then return its record', async () => {
+    expect(await service.remove(1)).toEqual({
+      id: 1,
+      ...dto,
+    });
   });
 });
