abc9e3eb15dae73f1cc6f39f07c79161852bd3fe
hatemBay <bayoudhhatem5@gmail.com>
2023-08-02 15:49:22 +0100

    added User repository mock, added create function test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 describe('UsersService', () => {
   let service: UsersService;
 
+  const mockUsersRepository = {
+    create: jest.fn().mockImplementation((dto) => dto),
+    save: jest.fn().mockImplementation((dto) => {
+      return {
+        id: Date.now(),
+        ...dto,
+      };
+    }),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUsersRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
@@ -14,5 +32,21 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a new user record and return it', async () => {
+    const dto = {
+      username: 'Hatem',
+      password: '123',
+      roles: [{ name: 'ADMIN' }],
+    };
+
+    expect(await service.create(dto)).toEqual({
+      id: expect.any(Number),
+      ...dto,
+    });
+
+    // * Asserting that the create and save methods are called would probably cause problems
+    // * if we use insert from the repository instead of create, so better not assert
   });
 });
